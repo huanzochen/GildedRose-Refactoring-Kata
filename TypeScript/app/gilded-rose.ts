e478d2b675638cfbb6b707e66ec65e59342f8f1d
ziquanh19 <cy94295@gmail.com>
2026-07-18 18:31:00 +0800

    aaa

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -24,8 +24,39 @@ export class GildedRose {
     this.items = items;
   }
 
+  // backstage pass
+  // if (item.sellIn <= 10 && item.sellIn > 5) item.quality = item.quality + 2;
+  //   else if (item.sellIn <= 5 && item.sellIn >= 0)
+  //     item.quality = item.quality + 3;
+  //   else if (item.sellIn < 0) item.quality = 0;
+  //   else {
+  //     item.quality = item.quality + 1;
+  //   }
+
+  updateItemAgedBrie(item: Item) {
+    const isExpired = item.sellIn <= 0;
+
+    const updateQuality = (item) => {
+      if (item.quality >= 50 || item.quality <= 0) return;
+
+      if (item.quality < 50)
+        isExpired ? (item.quality += 2) : (item.quality += 1);
+    };
+
+    const updateSellIn = (item) => {
+      item.sellIn -= 1;
+    };
+
+    updateQuality(item);
+    updateSellIn(item);
+  }
+
   updateItem(item: Item) {
     if (item.name === ItemType.SULFURAS) return;
+    if (item.name === ItemType.AGED_BRIE) {
+      this.updateItemAgedBrie(item);
+      return;
+    }
 
     if (
       item.name != ItemType.AGED_BRIE &&
