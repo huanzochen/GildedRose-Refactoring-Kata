f15c3c272991b3af1c4f92b9757b2bf2c1db4a22
ziquanh19 <cy94295@gmail.com>
2026-07-18 17:56:33 +0800

    aaa

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -25,14 +25,14 @@ export class GildedRose {
   }
 
   updateItem(item: Item) {
+    if (item.name === ItemType.SULFURAS) return;
+
     if (
       item.name != ItemType.AGED_BRIE &&
       item.name != ItemType.BACKSTAGE_PASSES
     ) {
       if (item.quality > 0) {
-        if (item.name != ItemType.SULFURAS) {
-          item.quality = item.quality - 1;
-        }
+        item.quality = item.quality - 1;
       }
     } else {
       if (item.quality < 50) {
@@ -51,16 +51,12 @@ export class GildedRose {
         }
       }
     }
-    if (item.name != ItemType.SULFURAS) {
-      item.sellIn = item.sellIn - 1;
-    }
+    item.sellIn = item.sellIn - 1;
     if (item.sellIn < 0) {
       if (item.name != ItemType.AGED_BRIE) {
         if (item.name != ItemType.BACKSTAGE_PASSES) {
           if (item.quality > 0) {
-            if (item.name != ItemType.SULFURAS) {
-              item.quality = item.quality - 1;
-            }
+            item.quality = item.quality - 1;
           }
         } else {
           item.quality = item.quality - item.quality;
